5856c4ce6f5ce1d049765ac455cdf3fd8aae1ad9
oleg.i@ideasoft.io <oleg.i@ideasoft.io>
2021-11-23 00:59:35 +0200

    created giphy component and pagination

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'GiphyTest'`, () => {
+  it(`should have as title 'my-giphy'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GiphyTest');
+    expect(app.title).toEqual('my-giphy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('GiphyTest app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-giphy app is running!');
   });
 });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  giphyApiKey: 'qBd3vtqOcmw6JLwub6YAHrXhhZe3U2nC'
 };
 
 /*
